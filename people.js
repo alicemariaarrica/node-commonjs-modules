948e75fdbd908cec87f631c69bc66c8d51e74705
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-29 16:19:53 +0100

    people.js-commit

---
+++ people.js
@@ -0,0 +1,5 @@
+//3 - people.js*
+//Importa la tua funzione da names.js
+//Importa la tua funzione da hobbies.js
+//Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
+//All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
