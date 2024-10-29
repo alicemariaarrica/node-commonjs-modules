539af40b4780d82757ee6e0872b953887b74cfcf
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-29 16:51:40 +0100

    adjustments-name-js-commit

--- names.js
+++ names.js
@@ -2,10 +2,10 @@
 //Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 //Esporta la funzione dal file.
 
-function createFullname(firstName, lastName){
+function createFullName(firstName, lastName){
     return {
         firstName: firstName,
         lastName: lastName
     };
 }
-module.exports = createName;
+module.exports = createFullName;
