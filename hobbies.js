d680faabfec6ce2e1e8686380b1fa2eab31e3804
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-29 16:16:36 +0100

    function-hobbies.js-commit

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+//2 - hobbies.js
+//Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un oggetto con una proprietà hobbies, che è un array di hobby.
+//Esporta la funzione dal file.
+
+function createHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return {
+      hobbies: [hobbyOne, hobbyTwo, hobbyThree]
+    };
+  }
+  
