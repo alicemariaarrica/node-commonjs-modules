dda635526dc33a209ee8e0bbdd0151a95f418d8a
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-29 16:42:28 +0100

    import-functions-commit

--- people.js
+++ people.js
@@ -3,3 +3,6 @@
 //Importa la tua funzione da hobbies.js
 //Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies.
 //All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+const createName=require('./names')
+const createHobbies=require('./hobbies')
