f4e76492a8d8def8b93bee1c4bd3f5c5e3de0c7c
alicemariaarrica <alicemariaarrica@gmail.com>
2024-10-29 16:04:55 +0100

    function-&-object-commit-name.js-commit

---
+++ names.js
@@ -0,0 +1,10 @@
+//1 - names.js
+//Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+//Esporta la funzione dal file.
+
+function createFullname(firstName, lastName){
+    return {
+        firstName: firstName,
+        lastName: lastName
+    };
+}
